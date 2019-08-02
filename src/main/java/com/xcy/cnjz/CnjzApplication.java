3e5866ecc6649c83ff4d7b86ff4db17e1f87ebf3
shikaiheng <874716257@qq.com>
2019-08-02 08:48:04 +0800

    自助建站项目框架

---
+++ src/main/java/com/xcy/cnjz/CnjzApplication.java
@@ -0,0 +1,13 @@
+package com.xcy.cnjz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnjzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnjzApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xcy/cnjz/CnjzApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xcy.cnjz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CnjzApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
